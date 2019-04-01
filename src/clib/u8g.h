80f0ae03643d6e92c4691e3e884c69aeefbc616c
Kajetan Rzepecki <k@spartan.works>
2019-04-01 03:27:30 +0200

    Added support for I2C SSD1139 OLED for ESP32.

--- src/clib/u8g.h
+++ src/clib/u8g.h
@@ -697,6 +697,8 @@ uint8_t u8g_com_raspberrypi_ssd_i2c_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val,
 uint8_t u8g_com_stm32duino_hw_spi_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr);     /* u8g_com_stm32duino_hw_spi.cpp */
 uint8_t u8g_com_stm32duino_ssd_i2c_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr);    /* u8g_com_stm32duino_ssd_i2c.cpp */
 
+uint8_t u8g_com_esp32_ssd_i2c_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr);    /* u8g_com_esp32_ssd_i2c.cpp */
+
 
 /*
   Translation of system specific com drives to generic com names
@@ -864,6 +866,13 @@ defined(__18CXX) || defined(__PIC32MX)
 #define U8G_COM_SSD_I2C u8g_com_stm32duino_ssd_i2c_fn
 #endif
 #endif
+
+#ifndef U8G_COM_SSD_I2C
+#if defined(ARDUINO_ARCH_ESP32)
+#define U8G_COM_SSD_I2C u8g_com_esp32_ssd_i2c_fn
+#endif
+#endif
+
 
 #ifndef U8G_COM_SSD_I2C
 #define U8G_COM_SSD_I2C u8g_com_null_fn
