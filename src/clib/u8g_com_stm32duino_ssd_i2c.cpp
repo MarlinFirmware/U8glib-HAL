94de74814a89852067fd8b9cce6abda931b9fb1e
Tanguy Pruvot <tpruvot@users.noreply.github.com>
2019-09-01 04:36:17 +0200

    Fix i2c and pointer warnings (#12)

--- src/clib/u8g_com_stm32duino_ssd_i2c.cpp
+++ src/clib/u8g_com_stm32duino_ssd_i2c.cpp
@@ -49,25 +49,26 @@ uint8_t u8g_com_stm32duino_ssd_i2c_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, 
 
     case U8G_COM_MSG_WRITE_SEQ:
     {
+      uint8_t* dataptr = (uint8_t*)arg_ptr;
       #ifdef I2C_MAX_LENGTH
         while (arg_val > 0) {
           Wire.beginTransmission(0x3c);
           Wire.write(control);
           if (arg_val <= I2C_MAX_LENGTH) {
-            Wire.write((uint8_t *) arg_ptr, arg_val);
+            Wire.write(dataptr, arg_val);
             arg_val = 0;
           }
           else {
-            Wire.write((uint8_t *) arg_ptr, I2C_MAX_LENGTH);
+            Wire.write(dataptr, I2C_MAX_LENGTH);
             arg_val -= I2C_MAX_LENGTH;
-            arg_ptr += I2C_MAX_LENGTH;
+            dataptr += I2C_MAX_LENGTH;
           }
           Wire.endTransmission();
         }
       #else
         Wire.beginTransmission(0x3c);
         Wire.write(control);
-        Wire.write((uint8_t *) arg_ptr, arg_val);
+        Wire.write(dataptr, arg_val);
         Wire.endTransmission();
       #endif // I2C_MAX_LENGTH
       break;
